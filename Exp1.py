918b7584ddab4b1f53efb40dfc91cd502eccabcf
jyotikanase268-star <jyotikanase268@gmail.com>
2026-02-05 11:21:09 +0530

    python exp1

---
+++ Exp1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 05:29:39 2026
+
+@author: User
+"""
+name = input("Enter your name:")
+age = input("Enter your age:")
+student_class = input("Enter your class:")
+divison = input("Enter your divison:")
+roll_no = input("Enter your roll number:")
+batch = input ("Enter your batch:")
+print("\n-----Student Information-----")
+print("Name:",name)
+print("Age:",age)
+print("class:", student_class)
+print("Divison:", divison)
+print("Roll number:", roll_no)
+print("Batch:", batch)
