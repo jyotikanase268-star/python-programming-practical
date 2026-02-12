07fb4ead83345ea65593da115b8ba3dd8fa6f01b
jyotikanase268-star <jyotikanase268@gmail.com>
2026-02-12 11:11:41 +0530

    python programming exp 2

---
+++ Exp2(B).py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 10:18:50 2026
+
+@author: User
+"""
+
+n = int(input("Enter number:"))
+fact = 1
+for i in range(1,n+1):
+    fact = fact*i
+    print("factorial:",fact)
+    
